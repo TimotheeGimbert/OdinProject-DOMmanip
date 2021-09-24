aae681342041c0d7bf221dfc171a4637e8c8d3e1
TimotheeGimbert <timothee.gimbert@free.fr>
2021-09-24 17:16:43 +0200

    Add para with css style inner HTML

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ const container = document.querySelector('#container'); // selects element #cont
 
 const content = document.createElement('div'); // creates a div
 content.classList.add('contentClass'); // adds a class to that div
-content.textContent = 'This is a new JS generated content'; // adds text to that div
+content.textContent = "This is a new JS generated content"; // adds text to that div
 
 container.appendChild(content); // inserts the div to the selected element #container
 
+
+const para = document.createElement('p'); // creates a p
+para.style.cssText = "color: red;"; // add css style within html tag
+para.textContent = "I am a red 'p'";
+
+container.appendChild(para);
