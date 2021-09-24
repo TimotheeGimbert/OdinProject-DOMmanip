9bfa2465c87af496918d7db407da81667c5b3847
TimotheeGimbert <timothee.gimbert@free.fr>
2021-09-24 17:31:08 +0200

    Add div with childs

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ para.style.cssText = "color: red;"; // add css style within html tag
 para.textContent = "I am a red 'p'";
 
 container.appendChild(para);
+
+const title2 = document.createElement('h3');
+title2.style.cssText = "color: blue;";
+title2.textContent = "I'm a blue title h3";
+
+container.appendChild(title2);
+
+
+
+
+const div = document.createElement('div');
+div.style.cssText = "border: 3px solid black; background-color: pink;";
+
+const divH1 = document.createElement('h1');
+const divP = document.createElement('p');
+divH1.textContent = "I'm a h1 inside a pink div";
+divP.textContent = "I'm a p inside the same pink div";
+
+div.appendChild(divH1);
+div.appendChild(divP);
+
+container.appendChild(div);
